d19dde7d2c6d13a17882292f07f2084cc05ea60b
DESKTOP-Dragon2\holod <holodniak.e@gmail.com>
2023-05-21 15:12:10 +0300

    add home basic styles

--- wp-content/themes/diplom/page-home.php
+++ wp-content/themes/diplom/page-home.php
@@ -6,5 +6,14 @@
 
 <?php get_header(); ?>
 
+<section class="post">
+	<div class="post__container">
+		<div class="post__content">
+			<?php while( have_posts() ) : the_post();
+			the_content();
+			endwhile; ?>
+		</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
