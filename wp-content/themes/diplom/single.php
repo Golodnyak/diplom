c74c92dbba85de7b8a5be604282af9c49f0107b9
DESKTOP-Dragon2\holod <holodniak.e@gmail.com>
2023-05-21 14:33:08 +0300

    add basic styles

--- wp-content/themes/diplom/single.php
+++ wp-content/themes/diplom/single.php
@@ -7,9 +7,15 @@
 
 <?php echo get_template_part('template-parts/breadcrumbs');?>
 
-<?php while( have_posts() ) : the_post();
-the_content();
-endwhile; ?>
+<section class="post">
+	<div class="post__container">
+		<div class="post__content">
+			<?php while( have_posts() ) : the_post();
+			the_content();
+			endwhile; ?>
+		</div>
+	</div>
+</section>
 
 
 <?php get_footer(); ?>
